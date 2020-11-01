cee6de1ae7a22d1a9956e7687ec9cd0b94f4d426
Documenter.jl <documenter@juliadocs.github.io>
2020-11-01 19:55:07 +0000

    build based on 631ccff

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DiskCollections","category":"page"},{"location":"#DiskCollections","page":"Home","title":"DiskCollections","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DiskCollections]","category":"page"}]
+}
